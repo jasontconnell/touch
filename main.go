31f53b05aab5dc03794a157bd2683754bd397c72
Jason Connell <jason.connell@herodigital.com>
2021-08-25 18:37:16 -0400

    Update how a new file is created

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 
 	var err error
 	if sterr != nil {
-		_, err = os.OpenFile(p, os.O_CREATE, os.ModePerm)
+		_, err = os.Create(p)
 	} else if st.IsDir() {
 		err = filepath.Walk(p, func(path string, info os.FileInfo, err error) error {
 			if info.IsDir() {
